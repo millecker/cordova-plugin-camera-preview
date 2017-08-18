1d8e28fdaa4449ee6451dfe08297187bc652d0e0
Martin Illecker <martin.illecker@sis-consulting.eu>
2017-08-18 08:41:49 +0200

    Add SizeF import

--- src/android/CameraPreview.java
+++ src/android/CameraPreview.java
@@ -14,6 +14,7 @@ import android.widget.FrameLayout;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.util.SizeF;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
